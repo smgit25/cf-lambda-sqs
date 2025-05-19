0096bf1e609eb346571b0210246b7a97d4439053
smgit25 <swatick27m@outlook.com>
2025-05-19 21:57:54 +0530

    test-KV

--- worker/src/index.js
+++ worker/src/index.js
@@ -1,5 +1,6 @@
 export default {
-    async fetch(request) {
+    async fetch(request, env) {
+      await env.MY_KV.put("greeting", "Hello from Cloudflare KV!");
       return new Response("Hello, World!", {
         headers: { "Content-Type": "text/plain" },
       });
