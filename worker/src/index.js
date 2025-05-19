6be2b26bdbb79033303176412781f233819f8316
smgit25 <swatick27m@outlook.com>
2025-05-19 22:06:46 +0530

    test-KV

--- worker/src/index.js
+++ worker/src/index.js
@@ -1,8 +1,12 @@
 export default {
     async fetch(request, env) {
       await env.test.put("greeting", "Hello from Cloudflare KV!");
-      return new Response("Hello, World!", {
-        headers: { "Content-Type": "text/plain" },
-      });
+      const value = await env.test.get("greeting");
+
+    // Return the value to the user
+      return new Response(`Stored value: ${value}`);
+      // return new Response("Hello, World!", {
+      //   headers: { "Content-Type": "text/plain" },
+      // });
     },
   };
